2fae5514aa2ad6ef3a84d1916d2988c3468db55c
ChenYuMingN96074023 <47813931+ChenYuMingN96074023@users.noreply.github.com>
2019-03-05 16:34:11 +0800

    tttttest

--- main.c
+++ main.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
+int minus(int a,int c){return a-c;}
+int plus(int a,int c){  return a+c;}
+int multiply(int a, int c) {  return a*c; }
+int divided(int a, int c) { return a/c;}
+int (* show_type_attributes(char type) )(int,int){ 
+    int n[4];
+    n['+'] = 0;
+    n['-'] = 1;
+    n['*'] = 2;
+    n['/'] = 3;
+   int (*pFun[4])(int,int) = {plus , minus ,multiply ,divided};
+  return *pFun[n[(unsigned int)type]];
 
-int plus(int a, int b) { return a + b; }
-int minus(int a, int b) { return a - b; }
-int multiply(int a, int b) { return a * b; }
-int divided(int a, int b) { return a / b; }
+}
+//main() function
 int main()
 {
-//1223456
-	int a, c;
+    int a, c,d; 
 	char b;
 	printf("key a Function \nEX: 1 + 1\n");
-	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+	scanf("%i %c %i", &a, &b, &c); 
+    int (*f)(int,int);  // Declare a pointer to a function with two paramete
+    f = show_type_attributes(b); // f will take the return     
+    d=f(a,c);  
+    printf("the value is %d\n\r",d);
+    return 0;
 }

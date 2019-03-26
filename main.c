b839c6754d2cf2f900c9e079cf5d24f6d74579f9
ChenYuMingN96074023 <47813931+ChenYuMingN96074023@users.noreply.github.com>
2019-03-26 19:06:19 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,60 @@
 #include <stdio.h>
-int minus(int a,int c){return a-c;}
-int plus(int a,int c){  return a+c;}
-int multiply(int a, int c) {  return a*c; }
-int divided(int a, int c) { return a/c;}
-int (* show_type_attributes(char type) )(int,int){ 
-    int n[4];
-    n['+'] = 0;
-    n['-'] = 1;
-    n['*'] = 2;
-    n['/'] = 3;
-   int (*pFun[4])(int,int) = {plus , minus ,multiply ,divided};
-  return *pFun[n[(unsigned int)type]];
+#include <math.h>
 
+
+/*
+ * 計算平方
+ */
+double square(double x) {
+    return x * x;
 }
-//main() function
-int main()
-{
-    int a, c,d; 
-	char b;
-	printf("key a Function \nEX: 1 + 1\n");
-	scanf("%i %c %i", &a, &b, &c); 
-    int (*f)(int,int);  // Declare a pointer to a function with two paramete
-    f = show_type_attributes(b); // f will take the return     
-    d=f(a,c);  
-    printf("the value is %d\n\r",d);
+
+/*
+ * 計算三次方
+ */
+double cube(double x) {
+    return x * x * x;
+}
+
+/*
+ * 計算f()在(x,y)之間以n等份來逼近的積分數值,使用梯形法
+ */
+double integral(double (*f)(double), int n, double x, double y) {
+    int i;
+    double gap = (y - x) / n;
+    double fy1 = (*f)(x);
+    double fy2 = (*f)(x + gap);
+    double area = 0;
+    for (i = 0; i < n; i++) {
+        area += (fy1 + fy2) * gap / 2; // 使用梯形面積公式 
+        fy1 = fy2;
+        fy2 = (*f)(x + gap * (i + 1)); //下底
+    }
+    return area;
+}
+
+int main() {
+    char fun[100];
+    int n;
+    double x, y;
+    double (*f)(double); // f: a pointer to function(double) returning double
+    while (scanf("%99s",fun) != EOF) { // EOF定義於stdio.h內,一般系統上為-1
+        if (strcmp(fun,"square")==0) {
+            f = square;
+        } else if (strcmp(fun,"cube")==0) {
+            f = cube;
+        } else if (strcmp(fun,"sqrt")==0) {
+            f = sqrt; // sqrt is defined in math.h
+        } else if (strcmp(fun,"cbrt")==0) {
+            f = cbrt; // cbrt is defined in math.h
+        } else if (strcmp(fun,"end")==0) {
+            break;
+        } else {
+            printf("Unknown function\n");
+            continue;
+        }
+        scanf("%d%lf%lf", &n, &x, &y);
+        printf("Integral of %s from %lf to %lf is: %lf\n", fun, x, y, integral(f, n, x, y));
+    }
     return 0;
 }
